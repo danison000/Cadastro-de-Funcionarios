8cc65d47330901e6a01f4d1c88b00d628ec5522d
danison <danison.santos00@gmail.com>
2023-08-26 09:56:08 -0300

    fix configuration

--- src/main/java/com/dan/br/cadastrodefuncionario/CadastroDeFuncionarioApplication.java
+++ src/main/java/com/dan/br/cadastrodefuncionario/CadastroDeFuncionarioApplication.java
@@ -1,10 +1,11 @@
 package com.dan.br.cadastrodefuncionario;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastroDeFuncionarioApplication {
+public class CadastroDeFuncionarioApplication {	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDeFuncionarioApplication.class, args);
