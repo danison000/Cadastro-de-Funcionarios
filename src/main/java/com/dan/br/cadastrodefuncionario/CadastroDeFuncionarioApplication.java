8c0e69eab880a0f30c4dac64cd62fdc8daea8e1c
danison <danison.santos00@gmail.com>
2023-08-29 19:51:24 -0300

    Refatoração ok

--- src/main/java/com/dan/br/cadastrodefuncionario/CadastroDeFuncionarioApplication.java
+++ src/main/java/com/dan/br/cadastrodefuncionario/CadastroDeFuncionarioApplication.java
@@ -1,6 +1,5 @@
 package com.dan.br.cadastrodefuncionario;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
